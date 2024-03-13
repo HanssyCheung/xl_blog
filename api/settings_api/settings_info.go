fd56ed57edae85d0e00466a8d6ae46a2d77f062c
Hennessy <hanssy.zhx@x-giants.com>
2024-03-13 17:58:53 +0800

    封装响应

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -2,8 +2,14 @@ package settings_api
 
 import (
 	"github.com/gin-gonic/gin"
+	"xl-go-blog/models/res"
 )
 
 func (SettingsApi) SettingsInfoView(c *gin.Context) {
-	c.JSON(200, gin.H{"msg": "xxx"})
+	//c.JSON(200, gin.H{"msg": "xxx"})
+	//res.Ok(map[string]string{}, "xxx", c)
+	//res.OkWithData(map[string]string{
+	//	"id": "xxx",
+	//}, c)
+	res.FailWithCode(res.SettingsError, c)
 }
